114ecb88c46f35c288dc3a0bdc7a67dd4dcfb0de
chris <chris@aion.network>
2020-02-04 15:31:03 +0800

    add post

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-const { exec } = require('child_process');
+const { exec } = require('child_process')
+const http = require('http')
 const runtime = require('regenerator-runtime')
 const { AionKeystoreClient, AionLocalSigner } = require('@makkii/app-aion')
 const config = require('./config.json')
 const aion = new AionKeystoreClient()
 
 console.log('private_key ' + config.private_key)
+
+function post(data,fn){
+    
+    let content = JSON.stringify(data)
+    
+    let options={
+        host: '127.0.0.1',
+        port: 8545,
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': content.length
+        }
+    };
+    let req = http.request(options, (res) => {
+        let _data = '';
+        res.on('data', (chunk)=>{
+            _data += chunk;
+        });
+        res.on('end', ()=>{
+            callback(_data)
+        })
+    })
+    req.write(content);
+    req.end();
+}
 
 exec('xxd -plain dapp.jar', (err, stdout, stderr) => {
     if (err) {
@@ -19,7 +46,9 @@ exec('xxd -plain dapp.jar', (err, stdout, stderr) => {
         aion.signTransaction(unsigned_tx, new AionLocalSigner(), {
             private_key: config.private_key
         }).then((signed_tx) => {
-            console.log(signed_tx)
+            post(signed_tx,(res)=>{
+                console.log(res)
+            })
         })       
     }
 })
